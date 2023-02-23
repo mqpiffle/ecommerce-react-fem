a71ae1f18a0b5ab0bb0879c55806035243654186
Eric Elsner <ericelsnercodes@gmail.com>
2023-02-23 09:50:54 -0500

    initial project setup

---
+++ src/components/Header.js
@@ -0,0 +1,11 @@
+const Header = props => {
+    return (
+        <header className='header-container'>
+            <div className='header-left__container'>
+                {/* div with the brand and menu so we can change flex-direction on mobile view */}
+            </div>
+        </header>
+    )
+}
+
+export default Header
